e3baa87db715e8902c97db6faea56a9838cf6527
Bruno Mateus <bruno_205@hotmail.com>
2022-06-29 22:15:07 -0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { AiFillInstagram } from 'react-icons/ai'
 
 export function Footer() {
     return (
-        <footer className="p-4 bg-black  dark:bg-zinc-800   text-white  absolute bottom-0 w-full" >
+        <footer className="p-4 bg-black  dark:bg-zinc-800   text-white w-full" >
 
             <a href={"https://www.instagram.com/rodri.lucca/"} className="flex items-center gap-1 text-2xl" target="_blank"><AiFillInstagram /> <span className='text-base '>/@rodri.lucca</span></a>
 
