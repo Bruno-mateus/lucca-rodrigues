f4fe4bdda481c0d39e5dd5b7f809ef8ccb33627c
Bruno Mateus <bruno_205@hotmail.com>
2022-06-29 21:52:57 -0300

    darkmode fix

--- src/components/ToggleTheme.tsx
+++ src/components/ToggleTheme.tsx
@@ -1,7 +1,20 @@
+import { useEffect, useState } from "react";
 import { AiFillBulb } from "react-icons/ai";
-import { useDarkMode } from '../Hooks/HookTheme'
+
+
 export function ToggleTheme() {
-  const [colorTheme, setTheme] = useDarkMode()
+  const [theme, setTheme] = useState(localStorage.theme)
+  const colorTheme = theme === 'dark' ? 'light' : 'dark'
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+    root.classList.remove(colorTheme)
+    root.classList.add(theme);
+    localStorage.setItem('theme', theme)
+    console.log(root)
+    console.log(theme)
+  }, [theme, colorTheme])
+
   return (
     <button className="text-2xl text-orange-300 dark:text-white ease-in-out transition duration-300" onClick={() => setTheme(colorTheme)}>
       <AiFillBulb />
